95f50499ad6db47f3d6654770fe3e0d498e2b85f
Misel Evtimov <misel.evtimov@gmail.com>
2021-01-30 20:05:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,15 @@
+require 'pry'
+
 def find_even_values(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
+      binding.pry
       if src[row_index][element_index].even?
         puts [row_index][element_index]
-    end
-    element_index += 1
+      end
+      element_index += 1
     end
     row_index += 1
   end
